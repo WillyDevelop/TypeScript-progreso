e566fee68cadf757ece0d94792f7185af34abc06
willydevelop <willydev365@gmail.com>
2026-02-22 00:55:50 -0300

    prisma agregado y mysql iniciado

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -5,9 +5,24 @@ const PORT = 3000;
 
 app.use(express.json());
 
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+
+const users: User[] = [
+  { id: 1, name: "Willy", email: "willy@dev.com" },
+  { id: 2, name: "Ana", email: "ana@dev.com" }
+];
+
 app.get('/api/health', (req: Request, res: Response) => {
        res.status(200).json({ status: "ok", message: "Mi primer servidor TS está vivo!"})
  })
+
+ app.get('/api/users', (req: Request, res: Response) => {
+  res.status(200).json(users);
+});
 
 app.listen(PORT, () => {
         console.log(`servidor escuchando en http://localhost:${PORT}`)
