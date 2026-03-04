bc887656a61f8e45e8b22655f7c31b1de2e8bbf4
willydevelop <willydev365@gmail.com>
2026-03-04 17:03:10 -0300

    preparacion del backend para produccion

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,7 +3,8 @@ import cors from "cors"; // 1. Importamos la librería de permisos
 import userRoutes from "./routes/userRoutes.js";
 
 const app = express();
-const PORT = 3000;
+// Le decimos: "Usa el puerto que me dé el servidor en la nube, o el 3000 si estoy en mi PC"
+const PORT = process.env.PORT || 3000;
 
 app.use(cors()); // 2. ¡ESTA ES LA LLAVE! Abre la puerta a tu frontend
 app.use(express.json());
